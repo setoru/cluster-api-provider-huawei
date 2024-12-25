3ab72da5ef1b984847a69248007d53fe561a8a25
Paco Xu <paco.xu@daocloud.io>
2024-12-25 15:26:51 +0800

    init HuaweiCloudCluster

--- cmd/main.go
+++ cmd/main.go
@@ -149,6 +149,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "HuaweiCloudMachine")
 		os.Exit(1)
 	}
+	if err = (&controller.HuaweiCloudClusterReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "HuaweiCloudCluster")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
