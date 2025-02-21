c4ab05984372a91ada1aaef599d1af51ad44d946
ekko <lihai.tu@daocloud.io>
2025-02-21 16:29:47 +0800

    Fix instance creation timeout
    Signed-off-by: ekko <lihai.tu@daocloud.io>

--- pkg/services/ecs/instance.go
+++ pkg/services/ecs/instance.go
@@ -360,7 +360,8 @@ func (s *Service) CheckJob(jobId string) error {
 		JobId: jobId,
 	}
 
-	timeout := time.After(time.Minute)
+	wait := 2 * time.Minute
+	timeout := time.After(wait)
 	for {
 		resp, err := s.ECSClient.ShowJob(req)
 		if err != nil {
@@ -376,7 +377,7 @@ func (s *Service) CheckJob(jobId string) error {
 		case ecsModel.GetShowJobResponseStatusEnum().INIT, ecsModel.GetShowJobResponseStatusEnum().RUNNING:
 			select {
 			case <-timeout:
-				return fmt.Errorf("job timed out after 1 minute")
+				return fmt.Errorf("job timed out after %d seconds", wait)
 			case <-time.After(1 * time.Second):
 				continue
 			}
